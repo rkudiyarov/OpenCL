451d3c34b385795a4c80d22e730a0ba6277a07c1
Vadim Zakondyrin <thekondr@gmail.com>
2011-01-27 21:40:09 +0600

    V10 module

--- Foreign/OpenCL/Raw/V10.hs
+++ Foreign/OpenCL/Raw/V10.hs
@@ -1,2 +1,18 @@
+--------------------------------------------------------------------------------
+-- |
+-- Copyright : (c) [2011] Vadim Zakondyrin
+-- License   : BSD
+-- |
+--------------------------------------------------------------------------------
 
+module Foreign.OpenCL.Raw.V10
+     ( module Foreign.OpenCL.Raw.V10.Types
+     , module Foreign.OpenCL.Raw.V10.Error
+     , module Foreign.OpenCL.Raw.V10.Platform
+     , module Foreign.OpenCL.Raw.V10.Device
+     ) where
 
+import Foreign.OpenCL.Raw.V10.Types
+import Foreign.OpenCL.Raw.V10.Error
+import Foreign.OpenCL.Raw.V10.Platform
+import Foreign.OpenCL.Raw.V10.Device
