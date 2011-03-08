09a87e22a09a417c217456fc99909ab6e5d90be6
Vadim Zakondyrin <thekondr@gmail.com>
2011-03-08 21:19:30 +0600

    Added high-level api for Event

--- Foreign/OpenCL/V10.hs
+++ Foreign/OpenCL/V10.hs
@@ -15,6 +15,7 @@ module Foreign.OpenCL.V10
        , module Foreign.OpenCL.V10.Context
        , module Foreign.OpenCL.V10.CommandQueue
        , module Foreign.OpenCL.V10.Sampler
+       , module Foreign.OpenCL.V10.Event
        ) where
 
 import Foreign.OpenCL.V10.Error
@@ -24,5 +25,6 @@ import Foreign.OpenCL.V10.Device
 import Foreign.OpenCL.V10.Context
 import Foreign.OpenCL.V10.CommandQueue
 import Foreign.OpenCL.V10.Sampler
+import Foreign.OpenCL.V10.Event
 import Foreign.OpenCL.Raw.V10.Types
 import Foreign.OpenCL.Raw.V10.Enums
---
+++ Foreign/OpenCL/V10/Event.hs
@@ -0,0 +1,60 @@
+--------------------------------------------------------------------------------
+-- |
+-- Copyright : (c) [2011] Vadim Zakondyrin
+-- License   : BSD
+-- |
+--------------------------------------------------------------------------------
+
+module Foreign.OpenCL.V10.Event
+       ( ExecutionStatus (..)
+       , clWaitForEvents
+       , clGetEventCommandQueue
+       , clGetEventCommandType
+       , clGetEventCommandExecutionStatus
+       , clGetEventReferenceCount
+       , clRetainEvent
+       , clReleaseEvent
+       )
+       where
+
+import qualified Foreign.OpenCL.Raw.V10 as Raw
+import Foreign.OpenCL.Raw.C2HS
+
+import Foreign.OpenCL.V10.Error
+import Foreign.OpenCL.V10.Utils
+
+data ExecutionStatus = ExecutionStatusValid Raw.CLExecutionStatus
+                     | ExecutionStatusError Raw.CLError
+                     deriving (Show, Eq)
+
+clWaitForEvents :: [Raw.CL_event] -> IO ()
+clWaitForEvents evs =
+    do
+    let num_events = length evs
+    event_list <- newArray evs
+    retCode <- Raw.clWaitForEvents (Raw.cl_uint num_events) event_list
+    free event_list
+    clCheckError retCode $ return ()
+
+clGetEventCommandQueue :: Raw.CL_event -> IO Raw.CL_command_queue
+clGetEventCommandQueue = clGetInfoObject Raw.clGetEventInfo Raw.CLEventCommandQueue
+
+clGetEventCommandType :: Raw.CL_event -> IO Raw.CLCommandType
+clGetEventCommandType = clGetInfoEnum Raw.clGetEventInfo Raw.CLEventCommandType
+
+clGetEventCommandExecutionStatus :: Raw.CL_event -> IO ExecutionStatus
+clGetEventCommandExecutionStatus e =
+    do
+    retStatus <- clGetInfoIntegral Raw.clGetEventInfo Raw.CLEventCommandExecutionStatus e
+    if retStatus < 0
+        then return $ ExecutionStatusError (cToEnum retStatus)
+        else return $ ExecutionStatusValid (cToEnum retStatus)
+
+clGetEventReferenceCount :: (Integral i) => Raw.CL_event -> IO i
+clGetEventReferenceCount = clGetInfoIntegral Raw.clGetEventInfo Raw.CLEventReferenceCount
+
+clRetainEvent :: Raw.CL_event -> IO ()
+clRetainEvent = clRetain Raw.clRetainEvent
+
+clReleaseEvent :: Raw.CL_event -> IO ()
+clReleaseEvent = clRelease Raw.clReleaseEvent
