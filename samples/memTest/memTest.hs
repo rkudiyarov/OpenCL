02fdfbbe03bf31895156a79bffae9fef6706db9b
Vadim Zakondyrin <thekondr@crystalnix.com>
2011-03-09 18:57:16 +0600

    Added sample for testing mem objects

---
+++ samples/memTest/memTest.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Foreign.OpenCL.V10
+import Foreign.C.Types
+
+main :: IO ()
+main = do
+       pls <- clGetPlatformIDs
+       let pl = head pls
+       dvs <- clGetDeviceIDs pl [CLDeviceTypeAll]
+       c <- clCreateContext [ContextPlatform pl] dvs
+       let buf = [3.5, 11.5, 32.2, 34.7, 15.1] :: [CDouble]
+       m <- clCreateBufferFromList c [CLMemReadWrite, CLMemCopyHostPtr] buf
+       list <- clGetMemObjectList m :: IO [CDouble]
+       object_type <- clGetMemObjectType m
+       clReleaseMemObject m
+       clReleaseContext c
+       putStrLn $ "List of data:  " ++ show list
+       putStrLn $ "MemObjectType: " ++ show object_type
